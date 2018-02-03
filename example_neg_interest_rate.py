2976a42f7f9e5a04d891084967d61349bae97895
Caner Ates <caneris1992@gmail.com>
2018-02-03 11:10:46 +0100

    Add files via upload

--- example_neg_interest_rate.py
+++ example_neg_interest_rate.py
@@ -16,20 +16,23 @@ import matplotlib.pyplot as plt
 
 r = 0.03 # the interest rate which is going to be shocked
 
-P1, P3, epsilon = 0.6, 0.3, 0.005
+P1, P3, epsilon = 0.6, 0.3, 0.05
 P2, P4 = 1-P1, 0.5*(1-P3)
 
 Π_r = ((P1, P2, 0), (P2 - epsilon, P1, epsilon), (P4, P4, P3))
 
-z_r_vals = (2, 0.2, -1)
+z_r_vals = (1.5, 0.2, -3)
 
 
-# Compute the expected shock to the interest rate E_z
+# Compute the expected interest rate E_r
 E_z = expect.exp_val(z_vals=z_r_vals, P_z=Π_r)
-
+E_z
 # I am going to use E_z in an economy without any shocks to
 # the interest rate as a multiplikator for the interest rates in 
 # order to compare the differences in the savings behavior.
+
+M=25
+MM=250000
 
 cp_r_shock = canersProblem(r=r,
                            Π_r=Π_r,
@@ -38,31 +41,30 @@ cp_r_shock = canersProblem(r=r,
 
 capital_r_shock = capital_supply_r(cp_r_shock, 
                                    r_max=0.04,
-                                   grid_points=10,
-                                   T=100000)
+                                   grid_points=M,
+                                   T=MM)
 
-# since I need to multiply every grid point by E_z
-# I had to write another function
+# since I need to multiply every I had to write another function
 # where I can use the interest rate grid as an argument.
 # In that way I can multiply every gridpoint by E_z
 
 cp_no_r_shock = canersProblem()
 
-r_vals=np.linspace(1e-8, 0.04, 10)*E_z
+r_vals=np.linspace(1e-8, 0.04, M)*E_z
 
 capital_no_r_shock = capital_supply_r2(cp_no_r_shock,
                                        r_vals=r_vals,
-                                       T=100000)
+                                       T=MM)
 
-fig, ax = plt.subplots(figsize=(8,10))
+fig, ax = plt.subplots(figsize=(12,8))
 
-ax.plot(capital_no_r_shock, r_vals, label='no r shock')
-ax.plot(capital_r_shock, r_vals, label='r shock')
+ax.plot(capital_no_r_shock, np.linspace(1e-8, 0.04, M),'o-', alpha=0.6, label='no r shock')
+ax.plot(capital_r_shock, np.linspace(1e-8, 0.04, M),'^-', alpha=0.6, label='r shock')
 
 ax.set_yticks(np.arange(0, 0.038, .01))
-ax.set_xticks(np.arange(0, 3, 1))
+ax.set_xticks(np.arange(0, 0.5, 0.1))
 plt.ylim(0, 0.04)
-plt.xlim(0, 1.8)
+plt.xlim(0, 0.5)
 ax.set_xlabel('capital')
 ax.set_ylabel('interest rate')
 ax.grid(True)
